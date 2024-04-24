e1790af0a64f1e94a696b7fc00c01d3c043e8743
AdizadeMurad <bd7xw445u@code.edu.az>
2024-04-25 03:05:55 +0400

    add mywebsite

---
+++ Frontend/assets/admin/adminPanelGreatReads/admin.js
@@ -0,0 +1,115 @@
+const tbody = document.querySelector("tbody");
+
+function addCard(id, newstitle, image, description) {
+  const tr = document.createElement("tr");
+  tr.innerHTML += `
+  <td "><img id="table-image" src="${image}" alt=""></td>
+  <td ">${newstitle}</td>
+  <td>${description}</td>
+ <td><div class="general-btn-admin">    
+ <button  id="delete">delete <i class="fa-solid fa-trash"></i></button>
+  <a href="uptade.html#${id}"><button  id="edit">edit <i class="fa-solid fa-pencil"></i></button></a></div>
+
+ </td>
+`;
+  const del = tr.querySelector("#delete");
+  del.addEventListener("click", async function (e) {
+    e.preventDefault();
+    await deleteProducts(id);
+  });
+  tbody.appendChild(tr);
+}
+
+async function getAlldata() {
+  const res = await fetch("http://localhost:3000/greatread");
+  const data = await res.json();
+  return data;
+}
+
+async function generalUI() {
+  const data = await getAlldata();
+  data.forEach((item) =>
+    addCard(item.id, item.title, item.image, item.description)
+  );
+}
+generalUI();
+
+async function deleteProducts(id) {
+  const res = await fetch("http://localhost:3000/greatread/" + id, {
+    method: "delete",
+  });
+  const data = await res.json();
+  alert(id);
+  return data;
+}
+
+
+
+
+
+const form = document.querySelector("form");
+const name = document.querySelector("#name");
+const description = document.querySelector("#description");
+const imgURL = document.querySelector("#imgURL");
+
+const add = document.querySelector("#add");
+form.addEventListener("submit", async function (e) {
+  e.preventDefault();
+  await addProducts(name.value, imgURL.value, description.value);
+});
+
+async function addProducts(title, image, description) {
+  const res = await fetch("http://localhost:3000/greatread", {
+    method: "post",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      title,
+      image,
+      description,
+    }),
+  });
+  const data = await res.json();
+  return data;
+}
+
+
+async function fullfill() {
+  const allData = await getBYID(hash)
+  name.value=allData.title
+  description.value=allData.description
+  imgURL.value=allData.image
+}
+fullfill()
+
+
+form.addEventListener("submit", async function (e) {
+  e.preventDefault();
+ await uptadeProducts(hash,name.value,imgURL.value,description.value);
+ window.location.href = "./admin.Panel.html"
+});
+
+
+
+async function getBYID(id) {
+  const res = await fetch("http://localhost:3000/greatread/"+id);
+  const data = await res.json();
+  return data;
+}
+
+async function uptadeProducts(id,title, image, description) {
+  const res = await fetch("http://localhost:3000/greatread/"+id, {
+    method: "put",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      title,
+      image,
+      description,
+    }),
+  });
+  const data = await res.json();
+  return data;
+}
---
+++ Frontend/assets/admin/adminPanelGreatReads/uptade.js
@@ -0,0 +1,45 @@
+const form = document.querySelector("form");
+const name = document.querySelector("#name");
+const description = document.querySelector("#description");
+const imgURL = document.querySelector("#imgURL");
+const hash = window.location.hash.slice(1)
+
+async function fullfill() {
+  const allData = await getBYID(hash)
+  name.value=allData.title
+  description.value=allData.description
+  imgURL.value=allData.image
+}
+fullfill()
+
+
+form.addEventListener("submit", async function (e) {
+  e.preventDefault();
+ await uptadeProducts(hash,name.value,imgURL.value,description.value);
+ window.location.href = "./admin.Panel.html"
+});
+
+
+
+async function getBYID(id) {
+  const res = await fetch("http://localhost:3000/greatread/"+id);
+  const data = await res.json();
+  return data;
+}
+
+async function uptadeProducts(id,title, image, description) {
+  const res = await fetch("http://localhost:3000/greatread/"+id, {
+    method: "put",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      title,
+      image,
+      description,
+    }),
+  });
+  const data = await res.json();
+  return data;
+}
+
